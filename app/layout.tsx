aa9df8f6c90d9a009684d789f07ddcd31bc70407
Dika <nardika.kurniawan@lumoshive.com>
2024-05-12 20:47:49 +0700

    Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      <body
+        className={`${inter.className} bg-dark-2`}
+        suppressHydrationWarning={true}
+      >
+        {children}
+      </body>
     </html>
   );
 }
